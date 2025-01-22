c9e7f07ccb771285771e3f8326c7630ffa93fa1e
aidan-hadi <pwkg5823@leeds.ac.uk>
2025-01-22 14:01:19 +0000

    oled changes

--- sensor/main.py
+++ sensor/main.py
@@ -1,12 +1,15 @@
 from microbit import *
 import radio
 from data import *
+from OLED import *
 
 radio.config(channel=14, group=1, length=250)
 radio.on()
 
-sensor_id = "server140"
+sensor_id = "server142"
 server_id = "server14S"
 
 while True:
+    show("Device ID: {}".format(sensor_id), 0)
+    show("Ready To Recieve Data",7)
     recieve_data(sensor_id, server_id)
