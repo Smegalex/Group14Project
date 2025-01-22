b6660c9df2b4990823e40e75e338197e9efb38cb
aidan-hadi <pwkg5823@leeds.ac.uk>
2025-01-22 13:29:52 +0000

    changed outputs for the screen

--- sensor/data.py
+++ sensor/data.py
@@ -3,9 +3,9 @@ import radio
 
 def send_data(sender_id, receiver_id, data):
     print("Sending data...")
-
     message = {"sender_id": sender_id, "receiver_id": receiver_id, "data": data}
     radio.send(str(message) + "\n")
 
     print("Data sent!")
     print(str(message))
+
--- sensor/main.py
+++ sensor/main.py
@@ -1,20 +1,31 @@
 from microbit import *
 import radio
-
 from metrics import read_metrics
 from data import send_data
 
-radio.config(channel=14, group=1)
+radio.config(channel=14, group=1,length=250)
 radio.on()
 
-sensor_id = "server140"
+sensor_id = "server141"
 server_id = "server14C"
 
 while True:
-    if button_a.was_pressed():
+    incoming = radio.receive()
+    if incoming:
+        incomingDict = eval(incoming)
+        if incomingDict["sender_id"] == sensor_id and incomingDict["server_id"] == server_id:
+            if incomingDict["data"] == True:
+                data = read_metrics(sensor_id)
+                send_data(sensor_id, server_id, data)
+        else:
+            print("Data is incoming but is not for the device or is invalid")
+
+    if button_a.is_pressed():
         data = read_metrics(sensor_id)
         send_data(sensor_id, server_id, data)
 
+
+
     incoming = radio.receive()
     if incoming:
-        display.scroll(incoming)
+        print(incoming)
