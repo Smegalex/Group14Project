3322b2d9e3ab36f982728daaa022e6488202af34
Victor Norris <mktd6529@leeds.ac.uk>
2025-01-22 10:59:32 +0000

    Added my work from vscode

---
+++ server/controller/recieve.py
@@ -0,0 +1,24 @@
+from microbit import *
+import radio
+# Connect server to PC
+
+from microbit import *
+import radio
+
+radio.on()# Turn on the radio
+radio.config(channel=7, group=1)# Set the channel
+
+while True:
+    message = radio.receive()# Check for incoming messages
+    if message:
+        # Parse the message for the sender ID
+        if message.startswith("Kitronik1:"):
+            display.scroll("From K1: " + message[9:])  # Handle Kitronik 1
+        elif message.startswith("Kitronik2:"):
+            display.scroll("From K2: " + message[9:])  # Handle Kitronik 2
+        elif message.startswith("Kitronik3:"):
+            display.scroll("From K3: " + message[9:])  # Handle Kitronik 3
+        elif message.startswith("Kitronik4:"):
+            display.scroll("From K4: " + message[9:])  # Handle Kitronik 4
+        else:
+            display.scroll("Unknown: " + message)  # Handle unknown messages
