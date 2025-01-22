7f2d3f13253bea93ccc71e1de3ae2a2094007714
Smegalex <smegalsuper@gmail.com>
2025-01-22 14:09:38 +0200

    remove inbox + change receive spelling

--- server/computer/serialTests.py
+++ server/computer/serialTests.py
@@ -4,7 +4,7 @@ import time
 portName = "COM9"  # add more variables for other portnames
 
 personal_id = "server14S"
-target_id = "fy23ot"
+target_id = "sensor140"
 
 # Color codes for terminal text
 GREEN = "\033[1;32m"
@@ -30,14 +30,14 @@ def change_target(new_target):
     global target_id
     target_id = new_target
 
-# List to store unseen messages
+
 inbox = []
 
 
 def send_message():
     global receiver_id 
     user_input = input(
-        "Enter your message (type 'INBOX' to check messages): ")
+        "Enter your message: ")
 
     # If the user types 'INBOX', show all unseen messages
     if user_input.startswith("/"):
@@ -46,9 +46,6 @@ def send_message():
             username = username[1].strip()
             change_target(username)
             print(f"Messaging {receiver_id}")
-    elif user_input.upper() == "INBOX":
-        receive_messages()
-        show_inbox()
     else:
         message = str(create_message(user_input)) + "\n"
         connection.write(message.encode())
@@ -63,20 +60,9 @@ def receive_messages():
             if validate_message(msg_dict):
                 if msg_dict['receiver_id'] == personal_id:
                     inbox.append(msg_dict)
+                    print(f"{GREEN}{msg_dict["sender_id"]}:{GRAY} {msg_dict['msg']}")
         except:
             print("Invalid message received.")
-
-
-def show_inbox():
-    if inbox:
-        print(f"{GREEN}--- INBOX ---{GRAY}")
-        for msg in inbox:
-            print(f"{GREEN}{msg['msg']}{GRAY}")
-            print("\n")
-        inbox.clear()  # Clear the inbox after showing messages
-        print(f"{GREEN}--- END OF INBOX ---{GRAY}")
-    else:
-        print(f"{GREEN}Your inbox is empty.{GRAY}")
 
 
 def main():
