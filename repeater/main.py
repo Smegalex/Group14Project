004d64d5fd1a3f4e5d9a57189d7db108c3d31bfd
aidan-hadi <pwkg5823@leeds.ac.uk>
2025-01-24 14:43:07 +0000

    changed the solo repeaters

--- repeater/main.py
+++ repeater/main.py
@@ -9,6 +9,8 @@ radio.config(channel=14, group=1, length=250)
 radio.on()
 usedUUID = []
 while True:
+    if len[usedUUID] > 30:
+        usedUUID = []
     incoming = radio.receive()
     display.show("r")
     if incoming:
